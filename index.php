21da946abf998d4a800f925aa9d0f1217c40ff26
shinya-nexseed <shirai@nexseed.net>
2016-08-16 14:24:46 +0800

    create minus function

--- index.php
+++ index.php
@@ -8,4 +8,12 @@
 
     $result = add(1,1);
     echo $result;
+
+    function minus($num1, $num2) {
+        $result = $num1 - $num2;
+        return $result;
+    }
+
+    $result = minus(10,4);
+    echo $result;
 ?>
