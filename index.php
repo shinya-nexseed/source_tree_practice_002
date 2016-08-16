bbb3178d3c76f257050a05dee2915621bc80228f
shinya-nexseed <shirai@nexseed.net>
2016-08-16 15:40:00 +0800

    [WIP] add org_var_dump function.

--- index.php
+++ index.php
@@ -39,4 +39,12 @@
             echo '<br>';
         }
     }
+
+    function org_var_dump($val) {
+        if (DEBUG) {
+            echo '<pre>';
+            var_dump($val);
+            echo '<pre>';
+        }
+    }
 ?>
